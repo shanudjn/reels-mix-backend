19c3310031a112a58c06a1d249e7621bf4e8ddfc
Shahazad <shanudjn@gmail.com>
2025-02-15 17:49:56 +0530

    bring fe files to root

--- frontend/src/components/ui/Header.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Header = () => (
-  <header className="bg-white shadow-sm p-4">
-    <h1 className="text-2xl font-bold">Web Video Editor</h1>
-  </header>
-);
-
-export default Header;
